983460db0956abfe77bd46015919c2418bd46e38
Juan Munar <juan.munar-c@mail.escuelaing.edu.co>
2026-09-17 16:58:14 -0500

    feat: GREEN - B1 calcular puntaje base sumando pinos

--- src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
+++ src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
@@ -3,7 +3,12 @@ package edu.eci.dosw.bowling;
 import java.util.List;
 
 public class BowlingScorer {
+
     public int calculate(List<Frame> frames) {
-        throw new UnsupportedOperationException("Scorer aun no implementado");
+        int totalScore = 0;
+        for (Frame frame : frames) {
+            totalScore += frame.getPinsRolled();
+        }
+        return totalScore;
     }
 }
